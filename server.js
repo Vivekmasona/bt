5a0576d2b2f055ece106d553f461892ca0474a27
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-06-14 12:13:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 const express = require('express');
-const noble = require('noble');
+const bonjour = require('bonjour')();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 let devices = [];
 
-// State change handler
-noble.on('stateChange', (state) => {
-    if (state === 'poweredOn') {
-        noble.startScanning();
-    } else {
-        noble.stopScanning();
-    }
-});
-
-// Device discovery handler
-noble.on('discover', (peripheral) => {
+// Discover all available services
+bonjour.find({}, (service) => {
     const device = {
-        name: peripheral.advertisement.localName,
-        uuid: peripheral.uuid
+        name: service.name,
+        type: service.type,
+        port: service.port,
+        address: service.referer.address
     };
     devices.push(device);
 });
 
-// Endpoint to get the list of devices
 app.get('/devices', (req, res) => {
     res.json(devices);
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
